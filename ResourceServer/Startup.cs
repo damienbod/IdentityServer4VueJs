d6b1dcc485bdd2fa51fea175dd2bf1bfcf65f941
damienbod <damien_bod@hotmail.com>
2019-10-07 20:09:16 +0200

    removing unused code

--- ResourceServer/Startup.cs
+++ ResourceServer/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.IO;
 using Microsoft.AspNetCore.Authorization;
-using System.Security.Cryptography.X509Certificates;
 using Microsoft.EntityFrameworkCore;
 using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using Microsoft.Extensions.Hosting;
 using AspNet5SQLite.Model;
 using AspNet5SQLite.Repositories;
 
@@ -17,39 +12,16 @@ namespace AspNet5SQLite
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            _webHostEnvironment = webHostEnvironment;
         }
 
         public IConfiguration Configuration { get; }
 
-        private readonly IWebHostEnvironment _webHostEnvironment;
-
         public void ConfigureServices(IServiceCollection services)
         {
             var connection = Configuration.GetConnectionString("DefaultConnection");
-            var useLocalCertStore = Convert.ToBoolean(Configuration["UseLocalCertStore"]);
-            var certificateThumbprint = Configuration["CertificateThumbprint"];
-
-            X509Certificate2 cert;
-
-            if (_webHostEnvironment.IsProduction())
-            {
-                if (useLocalCertStore)
-                {
-                    using X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
-                    store.Open(OpenFlags.ReadOnly);
-                    var certs = store.Certificates.Find(X509FindType.FindByThumbprint, certificateThumbprint, false);
-                    cert = certs[0];
-                    store.Close();
-                }
-            }
-            else
-            {
-                cert = new X509Certificate2(Path.Combine(_webHostEnvironment.ContentRootPath, "damienbodserver.pfx"), "");
-            }
 
             services.AddDbContext<DataEventRecordContext>(options =>
                 options.UseSqlite(connection)
@@ -98,7 +70,6 @@ namespace AspNet5SQLite
                     policyUser.RequireClaim("scope", "dataEventRecords");
                 });
             });
-
 
             services.AddScoped<IDataEventRecordRepository, DataEventRecordRepository>();
             services.AddControllers()
